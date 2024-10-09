ed3653852d9f19f4a4d7df2de651d3b61957c95c
조기환 <warrenjo@naver.com>
2024-10-09 17:32:10 +0900

    [new] kotlin basic

---
+++ day3 - kotlin1/1. Basic/src/Main.kt
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    // 한 줄 주석
+
+    /* 여러 줄 주석
+    * 하나
+    * 둘
+    * 셋
+    * 넷
+    * */
+
+    /*1
+    2
+    3
+    4*/
+
+//    print("안녕하세요")
+//    print("안녕하세요")
+//    print("안녕하세요")
+
+    println("안녕하세요")
+    println("안녕하세요")
+    println("안녕하세요")
+
+    println("세미콜론 있음");
+    println("세미콜론 없음")
+    println("코틀린에서는 세미콜론 없는 게 정석")
+
+}
